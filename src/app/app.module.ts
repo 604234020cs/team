2e7d607a1b7abaa6ca52d461afc67652af2e530d
Wanida Baikodem <604234020@parichat.skru.ac.th>
2019-02-11 21:29:51 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { FriendrestProvider } from '../providers/friendrest/friendrest';
+import { FrienddetailPage } from '../pages/frienddetail/frienddetail';
+import { FriendlistPage } from '../pages/friendlist/friendlist';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    FrienddetailPage,
+    FriendlistPage
+  ],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    FrienddetailPage,
+    FriendlistPage
+  ],
+  providers: [
+    HttpClientModule,
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    FriendrestProvider
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { FriendlistPage } from '../friendlist/friendlist';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+  showFriendList(major:string){
+    this.navCtrl.push(FriendlistPage,
+      {major:major}
+      );
+  }
+
+}
